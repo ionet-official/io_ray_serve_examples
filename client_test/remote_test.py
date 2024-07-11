4e8cdafbdb1262296f060b8a440247c23bc1da8c
vzip <zip.logon@gmail.com>
2024-07-11 11:35:28 -0600

    clean

--- client_test/remote_test.py
+++ client_test/remote_test.py
@@ -14,11 +14,11 @@ async def make_request(session, url, data, auth_header):
     # don't forget to change cookies to your's
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Accept-Language': 'en-US;q=0.8,en;q=0.7',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
         'Content-Type': 'application/json',
-        'Cookie': '_hjSessionUser_3685343=eyJpZCI6IjEwNmFjMGM5LTRkYjQtNTJmNS1hMTA1LTlkNGRmYTcxN2M4MyIsImNyZWF0ZWQiOjE3MTEwMzc1NTI0NzgsImV4aXN0aW5nIjp0cnVlfQ==; rl_user_id=%22RudderEncrypt%3AU2FsdGVkX19KZ2A7tgYS4jI7PQMWAlDWmAgOQRLk0IQ%2BCWlZ3BZCqgSHLJDVLVqI%22; intercom-device-id-dwu95rf6=f8ab51ef-426b-4e20-aea8-8293b37f4e36; code-server-session=%24argon2id%24v%3D19%24m%3D65536%2Ct%3D3%2Cp%3D4%24%2FtSSKMmaHQyUDEV0omRARg%24FSqbsDvfnSd3A3Z%2Faq5stnSUHJDkVEA%2BMBtSgcZ%2BSCs',
+        'Cookie': '{your_cookie}',
     }
 
     start_time = time.time()
@@ -39,7 +39,7 @@ async def make_request(session, url, data, auth_header):
 
 async def main():
     #change url_id to your's
-    url_id = '1cd9a'
+    url_id = '00001'
     url = f"https://vscode-{url_id}.tunnels.io.systems/proxy/8000/"
 
     username = 'IDE_PASSWORD'
